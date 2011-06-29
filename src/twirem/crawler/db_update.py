59dc5871b86019d8c1a22b09b8a0ba284570a3d2
sowcod <sow.code.42+github@gmail.com>
2011-06-29 23:34:07 +0900

    set new UserFriend's start_date to time.time()

--- src/twirem/crawler/db_update.py
+++ src/twirem/crawler/db_update.py
@@ -50,8 +50,7 @@ def update_followers(user_id, followers):
 		o.save()
 	def followed(uid):
 		user.followers.create(
-				user = UserProfile.objects.get_or_create(user_id = uid)[0],
-				start_date = 0)
+				user = UserProfile.objects.get_or_create(user_id = uid)[0])
 	def refollowed(o, uid):
 		if not o.unfollow : return
 		# refollow
@@ -79,8 +78,7 @@ def update_friends(user_id, friends):
 		o.save()
 	def followed(uid):
 		user.friends.create(
-				friend = UserProfile.objects.get_or_create(user_id = uid)[0],
-				start_date = 0)
+				friend = UserProfile.objects.get_or_create(user_id = uid)[0])
 	def refollowed(o, uid):
 		if not o.unfollow : return
 		#refollow
